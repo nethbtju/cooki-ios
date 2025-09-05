5717faa4837366837c80e5fdcfb87352f1c24d92
nethbtju <neth.b.ss@gmail.com>
2025-09-06 09:45:02 +1000

    Initial Commit

---
+++ Cooki/CookiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookiApp.swift
+//  Cooki
+//
+//  Created by Neth Botheju on 6/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct CookiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
